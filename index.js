7ba110b9bd4fd918de051e6bf4b710815cfa5d7e
greatdane <vanderbyljeandre@gmail.com>
2023-08-29 15:25:38 +0200

    array

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function add(num1,num2){
 
 console.log( add(3, 4)   ) // should log 7
 console.log( add(9, 102) ) // should log 111
+
+// Create a function, getFirst(arr), that returns the first item in the array
+
+function getFirst(arr) {
+    return arr[0]
+}
+
+let firstCard = getFirst([10, 2, 5])
+
+console.log(firstCard)
+
+// Call it with an array as an argument to verify that it works
